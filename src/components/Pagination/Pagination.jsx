80693855a288173daccc7112a6649545496fb2a3
Cristian Espejel <tapiaxto@gmail.com>
2024-07-15 15:08:39 -0600

    pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
-const Pagination = () => (
-  <div className="flex justify-between items-center p-4">
-    <button className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-primary-600 rounded-lg hover:bg-primary-700">
-      Anterior
-    </button>
-    <button className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-primary-600 rounded-lg hover:bg-primary-700">
-      Siguiente
-    </button>
-  </div>
-);
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="flex justify-between items-center p-4">
+      <button
+        onClick={handlePrevious}
+        className={`inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-primary-600 rounded-lg hover:bg-primary-700 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
+        disabled={currentPage === 1}
+      >
+        Anterior
+      </button>
+      <span>{`${currentPage} / ${totalPages}`}</span>
+      <button
+        onClick={handleNext}
+        className={`inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-primary-600 rounded-lg hover:bg-primary-700 ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''}`}
+        disabled={currentPage === totalPages}
+      >
+        Siguiente
+      </button>
+    </div>
+  );
+};
 
 export default Pagination;
